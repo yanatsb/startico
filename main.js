b34e0deb78428efcec30947f6efc4424129aad17
yanatsb <cybina96@mail.ru>
2022-06-17 12:16:49 +0300

    add .

---
+++ main.js
@@ -0,0 +1,20 @@
+// let close = document.querySelector('.cross');
+// close.addEventListener('click', function(){
+//   close.classList.toggle('active');
+// });
+// document.querySelector('.burger__button').addEventListener('click', function(e) {
+//   e.preventDefault();  
+//   document.querySelector('.header__menu').classList.toggle('header__menu-active');
+//  })
+// document.querySelector('.header__list a').click(function(e) {
+//   e.preventDefault();  
+//    document.querySelector('.header__menu').classList.toggle('header__menu-active');
+//    })
+
+
+   let menuBtn = document.querySelector('.burger__button');
+  let menu = document.querySelector('.header__list');
+  menuBtn.addEventListener('click', function(){
+	menu.classList.toggle('active');
+  menuBtn.classList.toggle('active');
+});
